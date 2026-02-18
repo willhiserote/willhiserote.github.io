7f0ee3ce659a66aacd7a09736b55390652f94593
willhiserote <wdhiserote22@coe.edu>
2026-02-18 10:25:51 -0600

    Create data.js
    Data File

---
+++ data.js
@@ -0,0 +1,15 @@
+const products = [
+    { name: 'Wood Shelves', stock: 150 },
+    { name: 'Metal Shelves', stock: 100 },
+    { name: 'Hanging Rods', stock: 200 },
+    { name: 'Brackets', stock: 300 },
+    { name: 'Screw Packs', stock: 500 }
+];
+
+const orderHistory = [
+    // Sample: 3 builders' orders (add more from your list)
+    { builder: 'ABC Construction', date: '2025-01-15', items: { 'Wood Shelves': 20, 'Hanging Rods': 10 } },
+    { builder: 'XYZ Builders', date: '2025-02-10', items: { 'Metal Shelves': 15, 'Brackets': 30, 'Screw Packs': 50 } },
+    { builder: 'DEF Homes', date: '2025-03-05', items: { 'Wood Shelves': 25, 'Hanging Rods': 20, 'Brackets': 40 } },
+    // Add more historical orders here for better trends
+];
